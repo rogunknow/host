ea968dbbb46a0ffe336446ad4c7f26773602e2ce
rogunknow <68379937+rogunknow@users.noreply.github.com>
2020-08-19 17:46:03 +0530

    Updates

---
+++ precache-manifest.58ff5f30d643b72090216601d70e0981.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd864e0301613a7cb4c4651554af97a",
+    "url": "/react-app.git/index.html"
+  },
+  {
+    "revision": "4b272e3a236b16f357db",
+    "url": "/react-app.git/static/css/main.270d3189.chunk.css"
+  },
+  {
+    "revision": "55647d8473a1b98ce767",
+    "url": "/react-app.git/static/js/2.44fcf2f7.chunk.js"
+  },
+  {
+    "revision": "22fb575ad2de4508da753be9133cc97c",
+    "url": "/react-app.git/static/js/2.44fcf2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b272e3a236b16f357db",
+    "url": "/react-app.git/static/js/main.20dbd082.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/react-app.git/static/js/main.20dbd082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0d40be9305812bf156",
+    "url": "/react-app.git/static/js/runtime-main.265a73d2.js"
+  },
+  {
+    "revision": "275a2629582aa2af3469364c6748cf38",
+    "url": "/react-app.git/static/media/argon-react-white.275a2629.png"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/react-app.git/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "f7cb2d390498c2969b2b1050f3e024ce",
+    "url": "/react-app.git/static/media/creativetim-white-slim.f7cb2d39.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-app.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-app.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-app.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-app.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-app.git/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/react-app.git/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/react-app.git/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/react-app.git/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/react-app.git/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "ba449a0697388668ef05e043ffdeafca",
+    "url": "/react-app.git/static/media/img-2-1200x1000.ba449a06.jpg"
+  },
+  {
+    "revision": "0ec264785c9b6efbed2651d84105f436",
+    "url": "/react-app.git/static/media/landing.0ec26478.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/react-app.git/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/react-app.git/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/react-app.git/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/react-app.git/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/react-app.git/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "278a0d250cc46de3a4f65a0791479e71",
+    "url": "/react-app.git/static/media/profile.278a0d25.jpg"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/react-app.git/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/react-app.git/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/react-app.git/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/react-app.git/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/react-app.git/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
